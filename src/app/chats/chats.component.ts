66ae16009cf624da24edcf8635a5f85643415230
Muenda Khuthadzo <muendakhuthadzo@gmail.com>
2020-07-30 13:50:50 +0200

    chat

--- src/app/chats/chats.component.ts
+++ src/app/chats/chats.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-chats',
   templateUrl: './chats.component.html',
   styleUrls: ['./chats.component.css']
 })
-export class ChatsComponent implements OnInit {
+export class ChatsComponent implements OnInit, AfterViewInit  {
+  currentJustify = 'justified';
 
-  constructor() { }
+  @ViewChild('chatbox', {static: false}) chatbox: ElementRef;
+
+  constructor() {
+   
+   }
+
+   ngAfterViewInit() {
+       /* chats auto scrolls after DOM is ready
+    HINT * : This message should be called when a new message is sent or received
+    */
+   setTimeout(() => {
+    this._autoScroll()
+   }, 2000);
+    
+    
+  }
+
+    _autoScroll() {
+    this.chatbox.nativeElement.scroll({
+      top: this.chatbox.nativeElement.scrollHeight,
+      left: 0,
+      behavior: 'smooth'
+    });
+   }
 
   ngOnInit(): void {
+  
   }
 
 }
